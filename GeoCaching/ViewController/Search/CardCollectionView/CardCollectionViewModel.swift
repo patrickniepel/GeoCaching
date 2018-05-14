18fcfbc5381efe89181b5574ecf39089c3a4f78e
Marcel Hagmann <hagmann.marcel@yahoo.com>
2018-05-14 10:11:16 +0200

    Verloren gegangene Files wieder hinzugefügt

---
+++ GeoCaching/ViewController/Search/CardCollectionView/CardCollectionViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  CardCollectionViewModel.swift
+//  GeoCaching
+//
+//  Created by Philipp on 09.05.18.
+//  Copyright © 2018 Patrick Niepel. All rights reserved.
+//
+
+import UIKit
+
+struct CardCollectionViewModel{
+    
+    var backgroundImageView : UIImage?
+    
+    var icon1 : UIImage?
+    var icon1Text : String
+    
+    var icon2 : UIImage?
+    var icon2Text : String
+    
+    var cardTitle : String
+    var cardDescription : String
+    
+}
